c75d12f1c4a4c643e26b15dab04107ec7eda4621
Calpha <gabrielnsouto009@gmail.com>
2023-09-05 11:41:06 -0400

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,11 @@
 import os
 import sqlite3
 
-from app.database import controller
-import app.commands
-from app.extentions import clients, commands
-from app.database.database import engine, base
+from .database import controller
+import .commands
+from .extentions import clients, commands
+from .database.database import engine, base
+from .database.migrate import Migration
 
 from dotenv import load_dotenv
 load_dotenv(dotenv_path='app\.env')
@@ -13,10 +14,7 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 
 @clients.event
 async def on_ready():
-    print("attempting connection")
-    base.metadata.create_all(engine)
-    print("connected!")
-    print("sync commenced")
+    print("command sync commenced")
     await commands.sync()
     print("sync complete!")
     print(
@@ -24,5 +22,18 @@ async def on_ready():
     )
 
 def run():
+    print("attempting db connection")
+    
+    base.metadata.create_all(engine)
+    Migration.cfg.set_main_option('sqlalchemy.url', database_uri)
+    Migration.cfg.config_file_name = 'alembic.ini'
+    if not path.exists('migrations'):
+        Migration.init() 
+    
+    if not Migration.check():
+        Migration.revision()
+        Migration.upgrade()
+        
+    print("db connected!")
     print("app is now running")
     clients.run(TOKEN)
